7a0951ea474d807993b0abb42ab70c3ada312eb8
Rrrricky <eric.dufreche@hetic.net>
2018-04-11 11:36:58 +0200

    New category in header :construction:

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,8 @@
 <header class="app-header">
   <nav class="app-header-nav">
+    <a class="app-header-nav__item" href="posts">
+      <span>Annonces</span>
+    </a>
     <a class="app-header-nav__item" href="about">
       <span >Qui sommes-nous ?</span>
     </a>
