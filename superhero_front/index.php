af4b49817633df23002625aabe9f3ff25778ae79
Paul HOUDEBINE <paulhoud@gmail.com>
2018-04-12 18:17:01 +0200

    Add files via upload
    Final home design

---
+++ superhero_front/index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css"> 
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">   
+    <title>Document</title>
+</head>
+<body>
+    <nav class="head">
+        <div class="container">
+            <div class="vertical"><a href="#" class="selected">Accueil</a></div>
+            <div class="vertical"><a href="#">Classement</a></div>
+            <div class="vertical"><a href="#">À propos</a></div>
+            <div class="user"><a href="#"></a></div>
+        </div>
+    </nav>
+    <div class="jobs">
+        <div class="job">
+            <div class="icon"></div>    
+            <span class="texte-1">BRICOLAGE</span>
+            <span class="texte-2">Montage meuble IKEA - Montreuil 93100</span>
+            <span class="texte-3">Le 12/04 à partir de 14h</span>
+            <span class="texte-4">Jean, à 12h03</span>
+            <div class="action">Secourir</div>
+        </div>
+        <div class="job">
+            <div class="icon"></div>
+            <span class="texte-1">BRICOLAGE</span>
+            <span class="texte-2">Montage meuble IKEA - Montreuil 93100</span>
+            <span class="texte-3">Le 12/04 à partir de 14h</span>
+            <span class="texte-4">Jean, à 12h03</span>
+            <div class="action">Secourir</div>
+        </div>
+        <div class="job">
+            <div class="icon"></div>
+            <span class="texte-1">BRICOLAGE</span>
+            <span class="texte-2">Montage meuble IKEA - Montreuil 93100</span>
+            <span class="texte-3">Le 12/04 à partir de 14h</span>
+            <span class="texte-4">Jean, à 12h03</span>
+            <div class="action">Secourir</div>
+        </div>
+        <div class="job">
+            <div class="icon"></div>    
+            <span class="texte-1">BRICOLAGE</span>
+            <span class="texte-2">Montage meuble IKEA - Montreuil 93100</span>
+            <span class="texte-3">Le 12/04 à partir de 14h</span>
+            <span class="texte-4">Jean, à 12h03</span>
+            <div class="action">Secourir</div>
+        </div>
+        <div class="job">
+            <div class="icon"></div>
+            <span class="texte-1">BRICOLAGE</span>
+            <span class="texte-2">Montage meuble IKEA - Montreuil 93100</span>
+            <span class="texte-3">Le 12/04 à partir de 14h</span>
+            <span class="texte-4">Jean, à 12h03</span>
+            <div class="action">Secourir</div>
+        </div>
+        <div class="job">
+            <div class="icon"></div>
+            <span class="texte-1">BRICOLAGE</span>
+            <span class="texte-2">Montage meuble IKEA - Montreuil 93100</span>
+            <span class="texte-3">Le 12/04 à partir de 14h</span>
+            <span class="texte-4">Jean, à 12h03</span>
+            <div class="action">Secourir</div>
+        </div>
+        <div class="job">
+            <div class="icon"></div>    
+            <span class="texte-1">BRICOLAGE</span>
+            <span class="texte-2">Montage meuble IKEA - Montreuil 93100</span>
+            <span class="texte-3">Le 12/04 à partir de 14h</span>
+            <span class="texte-4">Jean, à 12h03</span>
+            <div class="action">Secourir</div>
+        </div>
+        <div class="job">
+            <div class="icon"></div>
+            <span class="texte-1">BRICOLAGE</span>
+            <span class="texte-2">Montage meuble IKEA - Montreuil 93100</span>
+            <span class="texte-3">Le 12/04 à partir de 14h</span>
+            <span class="texte-4">Jean, à 12h03</span>
+            <div class="action">Secourir</div>
+        </div>
+        <div class="job">
+            <div class="icon"></div>
+            <span class="texte-1">BRICOLAGE</span>
+            <span class="texte-2">Montage meuble IKEA - Montreuil 93100</span>
+            <span class="texte-3">Le 12/04 à partir de 14h</span>
+            <span class="texte-4">Jean, à 12h03</span>
+            <div class="action">Secourir</div>
+        </div>
+        <div class="job">
+            <div class="icon"></div>    
+            <span class="texte-1">BRICOLAGE</span>
+            <span class="texte-2">Montage meuble IKEA - Montreuil 93100</span>
+            <span class="texte-3">Le 12/04 à partir de 14h</span>
+            <span class="texte-4">Jean, à 12h03</span>
+            <div class="action">Secourir</div>
+        </div>
+        <div class="job">
+            <div class="icon"></div>
+            <span class="texte-1">BRICOLAGE</span>
+            <span class="texte-2">Montage meuble IKEA - Montreuil 93100</span>
+            <span class="texte-3">Le 12/04 à partir de 14h</span>
+            <span class="texte-4">Jean, à 12h03</span>
+            <div class="action">Secourir</div>
+        </div>
+        <div class="job">
+            <div class="icon"></div>
+            <span class="texte-1">BRICOLAGE</span>
+            <span class="texte-2">Montage meuble IKEA - Montreuil 93100</span>
+            <span class="texte-3">Le 12/04 à partir de 14h</span>
+            <span class="texte-4">Jean, à 12h03</span>
+            <div class="action">Secourir</div>
+        </div>
+
+    </div>
+        
+    
+    <footer class="foot">
+        <div class="buttons">
+            <div class="flag"><a href="#"></a></div>
+            <div class="note">1</div>
+            <div class="add">+</div>
+        </div>
+        <div class="container">
+            <div class="vertical"><a href="#">Qui sommes nous ?</a></div>
+            <div class="vertical"><a href="#">Contact et support</a></div>
+            <div class="vertical"><a href="#">Mentions légales</a></div>
+        </div>
+    </footer>
+</body>
+</html>
